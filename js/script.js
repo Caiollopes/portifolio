20d2fa621c3389636d2d722c7e493eb2942c0159
Caio Lopes <pess.caio@hotmail.com>
2024-03-13 21:20:10 -0300

    ajuste no menu responsivo

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
+const header = document.querySelector("header");
 
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 70);
+});
 
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+const menu = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menu.addEventListener("click", () => navbar.classList.toggle("active"));
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
